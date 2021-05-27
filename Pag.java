007f1e6bf6ac55cc8ed981a3167e45eca8da9309
Erick-Perez12 <82849144+Erick-Perez12@users.noreply.github.com>
2021-05-27 17:21:35 -0500

    Update and rename Person.java to Pag.java

---
+++ Pag.java
@@ -0,0 +1,19 @@
+package mi.server;
+
+public class Pag{
+	private String message;
+	private String numero;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+}
+
